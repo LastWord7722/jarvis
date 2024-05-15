93db00c6c271cddf493ec6125a4a7d8e042f79a3
LastWord7722 <lastwordmark@gmail.com>
2024-05-15 18:49:13 +0300

    add basic logic for this bot

---
+++ src/app/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+
+const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const commands = require(path.join(__dirname, 'commands', file));
+}
+
+console.log('hello world');
