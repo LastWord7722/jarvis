4508cf5a57b027b45437b5635ff83b72a0822597
LastWord7722 <lastwordmark@gmail.com>
2024-05-15 21:15:59 +0300

    add for test basic command

--- src/app/index.js
+++ src/app/index.js
@@ -1,10 +1,39 @@
+require('dotenv').config();
+const Discord = require('discord.js');
+// Using Intents class
+const client = new Discord.Client({ intents: [
+        Discord.GatewayIntentBits.Guilds,
+        Discord.GatewayIntentBits.GuildMessages
+    ]})
 const fs = require('fs');
 const path = require('path');
+const prefix = '!';
 
+//take all file from commands
 const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(file => file.endsWith('.js'));
-
 for (const file of commandFiles) {
     const commands = require(path.join(__dirname, 'commands', file));
 }
-console.log('denis hello this is git');
-console.log("update git to version 1.06.a");
+client.on('ready', () => {
+    console.log(`Я ЖИВУ11!`);
+});
+//event message
+client.on('messageCreate', (message) => {
+    //what this??
+    console.log(!message.guild)
+    console.log(message.author)
+    if (!message.guild) return;
+    if (message.author.bot) return;
+    // if (!message.content.startsWith(prefix)) return;
+    let command = message.content.toLocaleLowerCase().split(" ")[0].slice(prefix.length);
+    let params = message.content.split(" ").slice(1);
+    let cmd;
+    console.log(message)
+    console.log(command)
+    if (command === 'h') {
+        message.reply("Hey!.")
+    }
+});
+
+
+client.login(process.env.DISCORD_TOKEN);
