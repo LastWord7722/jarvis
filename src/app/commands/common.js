a5dba2bcbfbb6ab3d4696ea1de17d4a3ae4c6941
LastWord7722 <lastwordmark@gmail.com>
2024-05-15 21:59:33 +0300

    add base structure

--- src/app/commands/common.js
+++ src/app/commands/common.js
@@ -1,1 +1,7 @@
-console.log('hello common');
+module.exports = {
+    name: 'common',
+    description: 'Просто тестовая команда',
+    execute(message, args) {
+        message.reply('Это тестовая команда №1');
+    },
+};
--- src/app/index.js
+++ src/app/index.js
@@ -1,39 +1,46 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 // Using Intents class
-const client = new Discord.Client({ intents: [
+const client = new Discord.Client({
+    intents: [
         Discord.GatewayIntentBits.Guilds,
-        Discord.GatewayIntentBits.GuildMessages
-    ]})
+        Discord.GatewayIntentBits.GuildMessages,
+        Discord.GatewayIntentBits.MessageContent
+    ]
+});
 const fs = require('fs');
 const path = require('path');
-const prefix = '!';
+const prefix = process.env.PREFIX;
 
-//take all file from commands
+client.commands = new Discord.Collection();
+
+// Load commands
 const commandFiles = fs.readdirSync(path.join(__dirname, 'commands')).filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
-    const commands = require(path.join(__dirname, 'commands', file));
+    const command = require(path.join(__dirname, 'commands', file));
+    client.commands.set(command.name, command);
 }
+
 client.on('ready', () => {
-    console.log(`Я ЖИВУ11!`);
-});
-//event message
-client.on('messageCreate', (message) => {
-    //what this??
-    console.log(!message.guild)
-    console.log(message.author)
-    if (!message.guild) return;
-    if (message.author.bot) return;
-    // if (!message.content.startsWith(prefix)) return;
-    let command = message.content.toLocaleLowerCase().split(" ")[0].slice(prefix.length);
-    let params = message.content.split(" ").slice(1);
-    let cmd;
-    console.log(message)
-    console.log(command)
-    if (command === 'h') {
-        message.reply("Hey!.")
-    }
+    console.log(`Я живу`);
 });
 
+client.on('messageCreate', (message) => {
+    if (!message.guild || message.author.bot || !message.content.startsWith(prefix)) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+
+    const command = client.commands.get(commandName);
+
+    if (!command) return;
+
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('Произошла ошибка при выполнении команды!');
+    }
+});
 
 client.login(process.env.DISCORD_TOKEN);
